41c0d5b2d847ef9ee049b482432d3cd9507c80fe
caoquanghuu <147122360+caoquanghuu@users.noreply.github.com>
2023-10-13 18:28:46 +0700

    baitap2

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+var slider_img = document.querySelector('.content2img');
+var images = ['a.jpg', 'b.jpg', 'c.jpg', 'd.jpg'];
+var color1 = ["linear-gradient(to right,orange, white)","linear-gradient(to right,gray, white)","linear-gradient(to right,green, white)","linear-gradient(to right,pink, white)"];
+var i = 0;
+
+function prev(){
+	if(i <= 0) i = images.length;	
+	i--;
+	return setImg();			 
+}
+
+function next(){
+	if(i >= images.length-1) i = -1;
+	i++;
+	return setImg();			 
+}
+
+function setImg(){
+	slider_img.setAttribute('src', "images/"+images[i]);
+    document.getElementById('body1').style.backgroundImage = color1[i];
+}
