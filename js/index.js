55bfd56783fef890f5dbf6e5b6cbbda7bc8d6926
caoquanghuu <147122360+caoquanghuu@users.noreply.github.com>
2023-10-25 14:15:26 +0700

    add: email submit js

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("email-input-title");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
